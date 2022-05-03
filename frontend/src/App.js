6fe18419eb3b3d371ccfb2ba193431d8c9fdd076
Emin <eminshamshoian@gmail.com>
2022-05-02 20:21:40 -0700

    readded old captcha v3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,47 +12,54 @@ import PasswordResetPage from "./pages/PasswordResetPage";
 import UserListPage from "./pages/UserListPage";
 import UserEditPage from "./pages/UserEditPage";
 import ErrorPage from "./pages/ErrorPage";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 // for showing the 'new update available' banner and to register the service worker
 import ServiceWorkerWrapper from "./ServiceWorkerWrapper";
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <ServiceWorkerWrapper />
+    <GoogleReCaptchaProvider reCaptchaKey='6LdeibwfAAAAAKzYkKfvncVQsNR4B9bv_VzF7oiQ'>
+      <Router>
+        <Header />
+        <ServiceWorkerWrapper />
 
-      <main className='py-2'>
-        <Container>
-          <Switch>
-            <Route path='/' component={HomePage} exact />
-            <Route path='/search/:keyword' component={HomePage} exact />
-            <Route path='/page/:pageNumber' component={HomePage} exact />
-            <Route
-              path='/search/:keyword/page/:pageNumber'
-              exact
-              component={HomePage}
-            />
-            <Route path='/login' component={LoginPage} />
-            <Route path='/register' component={RegisterPage} />
-            <Route
-              path='/user/password/reset/:token'
-              component={PasswordResetPage}
-            />
-            <Route path='/profile' component={ProfilePage} />
-            <Route path='/user/confirm/:token' component={ConfirmPage} exact />
-            <Route path='/admin/userlist' component={UserListPage} exact />
-            <Route
-              path='/admin/userlist/:pageNumber'
-              component={UserListPage}
-              exact
-            />
-            <Route path='/admin/user/:id/edit' component={UserEditPage} />
-            <Route component={ErrorPage} />
-          </Switch>
-        </Container>
-      </main>
-    </Router>
+        <main className='py-2'>
+          <Container>
+            <Switch>
+              <Route path='/' component={HomePage} exact />
+              <Route path='/search/:keyword' component={HomePage} exact />
+              <Route path='/page/:pageNumber' component={HomePage} exact />
+              <Route
+                path='/search/:keyword/page/:pageNumber'
+                exact
+                component={HomePage}
+              />
+              <Route path='/login' component={LoginPage} />
+              <Route path='/register' component={RegisterPage} />
+              <Route
+                path='/user/password/reset/:token'
+                component={PasswordResetPage}
+              />
+              <Route path='/profile' component={ProfilePage} />
+              <Route
+                path='/user/confirm/:token'
+                component={ConfirmPage}
+                exact
+              />
+              <Route path='/admin/userlist' component={UserListPage} exact />
+              <Route
+                path='/admin/userlist/:pageNumber'
+                component={UserListPage}
+                exact
+              />
+              <Route path='/admin/user/:id/edit' component={UserEditPage} />
+              <Route component={ErrorPage} />
+            </Switch>
+          </Container>
+        </main>
+      </Router>
+    </GoogleReCaptchaProvider>
   );
 };
 
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   Form,
@@ -15,7 +15,7 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 import FormContainer from "../components/FormContainer";
 import "../styles/login-register.css";
-import ReCAPTCHA from "react-google-recaptcha";
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
 
 const LoginPage = ({ location, history }) => {
   const [showRedirectMsg, setShowRedirectMsg] = useState(false);
@@ -31,6 +31,26 @@ const LoginPage = ({ location, history }) => {
   const userLogin = useSelector((state) => state.userLogin);
   const { loading, userInfo, error } = userLogin;
   const storedInfo = JSON.parse(localStorage.getItem("userInfo"));
+
+  const { executeRecaptcha } = useGoogleReCaptcha();
+
+  // Create an event handler so you can call the verification on button click event or form submit
+  const handleReCaptchaVerify = useCallback(async () => {
+    if (!executeRecaptcha) {
+      console.log("Execute recaptcha not yet available");
+      return;
+    }
+
+    // eslint-disable-next-line
+    const token = await executeRecaptcha("yourAction");
+    // Do whatever you want with the token
+    // eslint-disable-next-line
+  }, []);
+
+  // You can use useEffect to trigger the verification as soon as the component being loaded
+  useEffect(() => {
+    handleReCaptchaVerify();
+  }, [handleReCaptchaVerify]);
 
   useEffect(() => {
     if (!location.search.includes("success") && userInfo)
@@ -133,13 +153,9 @@ const LoginPage = ({ location, history }) => {
       setShowLoading(false);
       setEmailSent(true);
       // store the name in localstorage
-      localStorage.setItem("EcommerceUserName", data.name); // store the user name, so that we can use it in the profile page to ask them to confirm email
+      localStorage.setItem("username", data.name); // store the user name, so that we can use it in the profile page to ask them to confirm email
     }
   };
-
-  function onChange(value) {
-    console.log("Captcha value:", value);
-  }
 
   // there are 2 sorts of forms to be shown, one when resetting password, and other when normal login
   if (!forgotPassword) {
@@ -336,10 +352,7 @@ const LoginPage = ({ location, history }) => {
                   >
                     I remember my password
                   </Button>
-                  <ReCAPTCHA
-                    sitekey='6LdeibwfAAAAAKzYkKfvncVQsNR4B9bv_VzF7oiQ'
-                    onChange={onChange}
-                  />
+
                   <Button
                     type='submit'
                     className='ms-auto'
